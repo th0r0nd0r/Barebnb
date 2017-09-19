6b79c6163a41a2847bbd4e999ea100af11c1124a
= <pjfritch1@gmail.com>
2017-09-19 14:44:38 -0700

    make SessionsController

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(user)
+      render 'api/users/show'
+    else
+      render json: ["Invalid username or password"], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render 'api/users/show'
+    else
+      render json: ["How can you log out that which has not logged in?"], status: 404
+    end
+  end
+
+
+end
