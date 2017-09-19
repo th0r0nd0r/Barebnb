dd0db8497cf810b8fdd31b1ba5bf60a6b0843f7b
= <pjfritch1@gmail.com>
2017-09-19 14:37:56 -0700

    make users controller, model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  def login!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end 
 end
