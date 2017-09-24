dc762499dd5e82bc1876c26c07e718bc5164512d
= <pjfritch1@gmail.com>
2017-09-24 15:23:42 -0700

    turn logo into link

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import GreetingContainer from '../greeting/greeting_container';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="header">
-    <h1 className="header-title">Barebnb</h1>
+    <Link to="/" className="header-link">
+      <h1 className="header-title">Barebnb</h1>
+    </Link>
     <GreetingContainer />
   </header>
 );
