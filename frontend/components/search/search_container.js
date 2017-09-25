bb96ae38b3c5f58d4bbffde4765681f5698102d7
= <pjfritch1@gmail.com>
2017-09-25 13:37:35 -0700

    removed capitalization on import statement

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 
 import { toArray } from '../../reducers/selectors';
-import Search from './Search';
+import Search from './search';
 
 const mapStateToProps = state => ({
   spots: Object.values(state.entities.spots)
