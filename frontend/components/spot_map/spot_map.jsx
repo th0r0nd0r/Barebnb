515db4c019088b2bb7b0eeac44cda38505260217
= <pjfritch1@gmail.com>
2017-09-25 15:51:10 -0700

    add event listeners to map

--- frontend/components/spot_map/spot_map.jsx
+++ frontend/components/spot_map/spot_map.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
 import MarkerManager from '../../util/marker_manager.js';
 
+const getCoordsObj = LatLng => ({
+  lat: LatLng.lat(),
+  lng: LatLng.lng()
+});
+
+const mapOptions = {
+  center: { lat: 37.989560, lng: -122.519999 }, // this is SF
+  zoom: 9
+};
+
 class SpotMap extends React.Component {
 
   componentDidMount() {
-    const mapOptions = {
-      center: { lat: 37.989560, lng: -122.519999 }, // this is SF
-      zoom: 9
-    };
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
     this.MarkerManager = new MarkerManager(this.map);
@@ -21,6 +27,26 @@ class SpotMap extends React.Component {
     this.MarkerManager.updateMarkers(this.props.spots);
   }
 
+  registerListeners() {
+    google.maps.event.addListener(this.map, 'idle', () => {
+      const { north, south, east, west } = this.map.getBounds().toJSON();
+      const bounds = {
+        northEast: { lat:north, lng: east },
+        southWest: { lat: south, lng: west } };
+      this.props.updateFilter('bounds', bounds);
+    });
+    google.maps.event.addListener(this.map, 'click', (event) => {
+      const coords = getCoordsObj(event.latLng);
+      this._handleClick(coords);
+    });
+  }
+
+  _handleClick(coords){
+    this.props.history.push({
+      search: `lat=${coords.lat}&lng=${coords.lng}`
+    });
+  }
+
   render() {
     return (
       <div className="map" ref={ map => this.mapNode = map }>
@@ -30,4 +56,4 @@ class SpotMap extends React.Component {
   }
 }
 
-export default SpotMap;
+export default withRouter(SpotMap);
