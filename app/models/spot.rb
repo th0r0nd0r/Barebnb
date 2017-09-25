4b8c3b5333a931857a5bf4f90b04c55d1696edb4
= <pjfritch1@gmail.com>
2017-09-25 13:28:52 -0700

    might be up to date

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -8,4 +8,20 @@ class Spot < ApplicationRecord
             :bed,
             :img_url,
             presence: true
+
+  def self.in_bounds(bounds)
+  # google map bounds will be in the following format:
+  # {
+  #   "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+  #   "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+  # }
+  #... query logic goes here
+    ne = [bounds["northEast"]["lat"].to_f, bounds["northEast"]["lng"].to_f]
+    sw = [bounds["southWest"]["lat"].to_f, bounds["southWest"]["lng"].to_f]
+
+    self
+    .find_each do |spot|
+      ((spot.lat.between?(sw[0], ne[0])) && (spot.lng.between?(sw[1], ne[1])))
+    end
+  end
 end
