0440620b86f3ed861e53dd2a381342073b51842f
= <pjfritch1@gmail.com>
2017-09-27 19:42:22 -0700

    bench show remounts when a new review is added

--- frontend/reducers/spots_reducer.js
+++ frontend/reducers/spots_reducer.js
@@ -1,14 +1,20 @@
-import { RECEIVE_SPOTS, RECEIVE_SPOT } from '../actions/spot_actions';
+import { RECEIVE_SPOTS, RECEIVE_SPOT, RECEIVE_REVIEW } from '../actions/spot_actions';
 import merge from 'lodash/merge';
 
 const SpotsReducer = (state = {}, action) => {
-  console.log("action:", action);
+  Object.freeze(state);
+  let newState = merge({}, state);
+
   switch(action.type) {
     case RECEIVE_SPOTS:
       return action.spots;
     case RECEIVE_SPOT:
       const newSpot = {[action.spot.id]: action.spot};
       return merge({}, state, newSpot);
+    case RECEIVE_REVIEW:
+      const review = action.review;
+      newState[review.spot_id].reviews.push(review);
+      return newState;
     default:
       return state;
   }
