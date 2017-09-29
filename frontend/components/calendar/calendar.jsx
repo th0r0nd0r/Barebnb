0eb8a9f0d59ab4ad8f65419de721d4492e66bc1e
= <pjfritch1@gmail.com>
2017-09-29 12:07:54 -0700

    date range

--- frontend/components/calendar/calendar.jsx
+++ frontend/components/calendar/calendar.jsx
@@ -11,22 +11,44 @@ class Calendar extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      startDate: moment()
+      startDate: moment(),
+      endDate: moment()
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeStart = this.handleChangeStart.bind(this);
+    this.handleChangeEnd = this.handleChangeEnd.bind(this);
   }
 
-  handleChange(date) {
+  handleChangeStart(date) {
     this.setState({
       startDate: date
     });
   }
 
+  handleChangeEnd(date) {
+    this.setState({
+      endDate: date
+    });
+  }
+
   render() {
-    return <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-    />;
+    return (
+      <div>
+        <DatePicker
+          selected={this.state.startDate}
+          selectsStart
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeStart}
+          />
+        <DatePicker
+          selected={this.state.endDate}
+          selectsEnd
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onChange={this.handleChangeEnd}
+          />
+      </div>
+    );
   }
 }
 
